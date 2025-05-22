b825c66bd509b395e331d375a1d40955221ed354
RosarioCatonde <williamfamozinho8@gmail.com>
2025-05-23 00:27:29 +0100

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,20 @@
+
+
+// Scroll Reveal
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.basica', {origin:'top' });
+ScrollReveal().reveal('.portfolio, .whatsapp-btn, .mensagem, .portfolio-caixa, .service-card, .credito', {origin:'bottom' });
+ScrollReveal().reveal('.mensagem, .padrao, .sobre-img', {origin:'left' });
+ScrollReveal().reveal('.btn, .premium, .sobre-conteudo', {origin:'right' });
+
+
+
+
+
+
